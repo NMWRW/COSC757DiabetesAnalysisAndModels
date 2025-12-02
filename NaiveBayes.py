66b835c27e34d8af3ac84e995631c9e73d59a52c
MartinaRL <mlampkin8@msn.com>
2025-12-02 17:34:08 -0500

    Added classification report

--- NaiveBayes.py
+++ NaiveBayes.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix, classification_report
 from sklearn.naive_bayes import GaussianNB
 import seaborn as sns
 
@@ -14,18 +14,17 @@ def nBayes(df):
     Y = df[target]
 
     X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=60)
-    scaler = MinMaxScaler()
-    X_train_scaled = scaler.fit_transform(X_train)
-    X_test_scaled = scaler.transform(X_test)
 
     nb_model = GaussianNB()
-    nb_model.fit(X_train_scaled, y_train)
-    nby_pred = nb_model.predict(X_test_scaled)
+    nb_model.fit(X_train, y_train)
+    nby_pred = nb_model.predict(X_test)
 
     print("Naive Bayes Accuracy: ", accuracy_score(y_test, nby_pred))
     print("Naive Bayes F1 Score: ", f1_score(y_test, nby_pred, average='weighted'))
     print("Naive Bayes Recall Score: ", recall_score(y_test, nby_pred, average='weighted'))
     print("Naive Bayes Precision Score: ", precision_score(y_test, nby_pred, average='weighted'))
+
+    print(classification_report(y_test, nby_pred))
 
     cm = confusion_matrix(y_test, nby_pred)
     print(cm)
