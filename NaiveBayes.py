ae7d933e5d8d518646a520e1e10e91c77332e349
MartinaRL <mlampkin8@msn.com>
2025-12-02 16:43:36 -0500

    Updated code for Naive Bayes algorithm

--- NaiveBayes.py
+++ NaiveBayes.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
@@ -7,93 +6,42 @@ from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_sc
 from sklearn.naive_bayes import GaussianNB
 import seaborn as sns
 
+def nBayes(df):
+
+    features = ["HighBP", "HighChol", "BMI", "Age", "GenHlth"]
+    target = df.columns[0]
+    X = df[features]
+    Y = df[target]
+
+    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=60)
+    scaler = MinMaxScaler()
+    X_train_scaled = scaler.fit_transform(X_train)
+    X_test_scaled = scaler.transform(X_test)
+
+    nb_model = GaussianNB()
+    nb_model.fit(X_train_scaled, y_train)
+    nby_pred = nb_model.predict(X_test_scaled)
+
+    print("Naive Bayes Accuracy: ", accuracy_score(y_test, nby_pred))
+    print("Naive Bayes F1 Score: ", f1_score(y_test, nby_pred, average='weighted'))
+    print("Naive Bayes Recall Score: ", recall_score(y_test, nby_pred, average='weighted'))
+    print("Naive Bayes Precision Score: ", precision_score(y_test, nby_pred, average='weighted'))
+
+    cm = confusion_matrix(y_test, nby_pred)
+    print(cm)
+
+    # Plot confusion matrix
+    plt.figure(figsize=(4, 3))
+    sns.heatmap(cm, annot=True, fmt='d', cmap='Blues')
+    plt.xlabel('Predicted Label')
+    plt.ylabel('Actual Label')
+    plt.title('Naive Bayes Confusion Matrix')
+    plt.show()
+
+
 df = pd.read_csv('Data/diabetes_012_health_indicators_BRFSS2015.csv')
+nBayes(df)
 df2 = pd.read_csv('Data/diabetes_binary_health_indicators_BRFSS2015.csv')
+nBayes(df2)
 df3 = pd.read_csv('Data/diabetes_binary_5050split_health_indicators_BRFSS2015.csv')
-
-#First dataframe
-X = df.drop(columns=['Diabetes_012'])
-Y = df['Diabetes_012']
-
-X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=60)
-scaler = MinMaxScaler()
-X_train_scaled = scaler.fit_transform(X_train)
-X_test_scaled  = scaler.transform(X_test)
-
-nb_model = GaussianNB()
-nb_model.fit(X_train_scaled,y_train)
-nby_pred = nb_model.predict(X_test_scaled)
-
-print("Naive Bayes Accuracy: ",accuracy_score(y_test, nby_pred))
-print("Naive Bayes F1 Score: ",f1_score(y_test, nby_pred, average='weighted'))
-print("Naive Bayes Recall Score: ",recall_score(y_test, nby_pred, average='weighted'))
-print("Naive Bayes Precision Score: ",precision_score(y_test, nby_pred, average='weighted'))
-
-cm=confusion_matrix(y_test, nby_pred)
-print(cm)
-
-# Plot confusion matrix
-plt.figure(figsize=(4, 3))
-sns.heatmap(cm, annot=True, fmt='d', cmap='Blues')
-plt.xlabel('Predicted Label')
-plt.ylabel('Actual Label')
-plt.title('Naive Bayes Confusion Matrix')
-plt.show()
-
-#Second dataframe
-X = df2.drop(columns=['Diabetes_binary'])
-Y = df2['Diabetes_binary']
-
-X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=60)
-scaler = MinMaxScaler()
-X_train_scaled = scaler.fit_transform(X_train)
-X_test_scaled  = scaler.transform(X_test)
-
-nb_model = GaussianNB()
-nb_model.fit(X_train_scaled,y_train)
-nby_pred = nb_model.predict(X_test_scaled)
-
-print("Naive Bayes Accuracy: ",accuracy_score(y_test, nby_pred))
-print("Naive Bayes F1 Score: ",f1_score(y_test, nby_pred, average='weighted'))
-print("Naive Bayes Recall Score: ",recall_score(y_test, nby_pred, average='weighted'))
-print("Naive Bayes Precision Score: ",precision_score(y_test, nby_pred, average='weighted'))
-
-cm=confusion_matrix(y_test, nby_pred)
-print(cm)
-
-# Plot confusion matrix
-plt.figure(figsize=(4, 3))
-sns.heatmap(cm, annot=True, fmt='d', cmap='Blues')
-plt.xlabel('Predicted Label')
-plt.ylabel('Actual Label')
-plt.title('Naive Bayes Confusion Matrix')
-plt.show()
-
-#Third dataframe
-X = df3.drop(columns=['Diabetes_binary'])
-Y = df3['Diabetes_binary']
-
-X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=60)
-scaler = MinMaxScaler()
-X_train_scaled = scaler.fit_transform(X_train)
-X_test_scaled  = scaler.transform(X_test)
-
-nb_model = GaussianNB()
-nb_model.fit(X_train_scaled,y_train)
-nby_pred = nb_model.predict(X_test_scaled)
-
-print("Naive Bayes Accuracy: ",accuracy_score(y_test, nby_pred))
-print("Naive Bayes F1 Score: ",f1_score(y_test, nby_pred, average='weighted'))
-print("Naive Bayes Recall Score: ",recall_score(y_test, nby_pred, average='weighted'))
-print("Naive Bayes Precision Score: ",precision_score(y_test, nby_pred, average='weighted'))
-
-cm=confusion_matrix(y_test, nby_pred)
-print(cm)
-
-# Plot confusion matrix
-plt.figure(figsize=(4, 3))
-sns.heatmap(cm, annot=True, fmt='d', cmap='Blues')
-plt.xlabel('Predicted Label')
-plt.ylabel('Actual Label')
-plt.title('Naive Bayes Confusion Matrix')
-plt.show()
+nBayes(df3)
